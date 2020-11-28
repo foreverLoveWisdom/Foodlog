87fcaadda85c21c89ced1a0c464736c2e43b26e5
Tien Do <domanhtien2011@gmail.com>
2020-11-28 20:20:21 +0700

    Finish basic features

---
+++ config/deploy.rb
@@ -0,0 +1,5 @@
+# config valid for current version and patch releases of Capistrano
+lock '3.4.0'
+
+set :application, 'Foodlog'
+set :repo_url, 'git@github.com:devdatta/contactbook.git' # Edit this to match your repository
