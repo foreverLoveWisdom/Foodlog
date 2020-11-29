a25672fe885bc6ad9cb6f674b057535c738ec79e
Tien Do <domanhtien2011@gmail.com>
2020-11-29 11:39:40 +0700

    Change from master to main branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.14.1'
 
 set :application, 'Foodlog'
 set :repo_url, 'git://github.com/foreverLoveWisdom/Foodlog.git' # Edit this to match your repository
-set :branch, :master
+set :branch, :main
 set :deploy_to, '/home/deploy/Foodlog'
 set :pty, true
 set :linked_files, %w[config/database.yml config/application.yml]
